9f3f0d9e0370e531f2bce386e31283b0f0f88ef5
Denis Novik <riskabeker@gmail.com>
2022-05-14 17:31:51 +0300

    - Added recipes for the buildings

--- js/building.js
+++ js/building.js
@@ -6,17 +6,35 @@ const Building = {
     TIMBER_WALL: {
       name: "Timber Wall",
       icon: "tibmer_basic_wall.webp",
-      cathegory: menu.list.BASIC_COMPONENTS.TIMBER
+      cathegory: menu.list.BASIC_COMPONENTS.TIMBER,
+      recipe: [{
+        ingridient: Ingridient.list.CARBON,
+        count: 25
+      }]
     },
     TIMBER_WALL_LARGE_WINDLOW: {
       name: "Extruded Timber Large Window",
       icon: "timber_extruded_window_wall.webp",
-      cathegory: menu.list.BASIC_COMPONENTS.TIMBER
+      cathegory: menu.list.BASIC_COMPONENTS.TIMBER,
+      recipe: [{
+        ingridient: Ingridient.list.CARBON,
+        count: 25
+      }, {
+        ingridient: Ingridient.list.GLASS,
+        count: 1
+      }]
     },
     TRADE_TERMINAL: {
       name: "Galactic Trade Terminal",
       icon: "trade_terminal.webp",
-      cathegory: menu.list.EQUIPMENT.PERMANENT
+      cathegory: menu.list.EQUIPMENT.PERMANENT,
+      recipe: [{
+        ingridient: Ingridient.list.FERRIT_M,
+        count: 25
+      }, {
+        ingridient: Ingridient.list.MICROCHIP,
+        count: 3
+      }]
     }
   }
 }
