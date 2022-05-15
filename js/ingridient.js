e27bbc042cf15fe1f84f094a1aad3ece2bf8a7d4
Denis Novik <riskabeker@gmail.com>
2022-05-15 22:14:15 +0300

    - Created initial code for ingridients panel

--- js/ingridient.js
+++ js/ingridient.js
@@ -1,4 +1,32 @@
 const Ingridient = {
+  toBuild: [],
+  /**
+   * Add building to to build list.
+   *
+   * @param biolding javascript object of the building to render.
+   */
+  addBuilding: (building) => {
+
+  },
+  /**
+   * Remove building from to build list.
+   *
+   * @param biolding javascript object of the building to render.
+   */
+  removeBuilding: (building) => {
+
+  },
+  /**
+   * Get url of the icon for the ingridient.
+   *
+   * @param ingridient - javascript object of the ingridient.
+   */
+  getIconUrl: (ingridient) => {
+    return "./images/ingridients/" + ingridient.icon;
+  }
+  /**
+   * List of ingridients available for crafting.
+   */
   list: {
     CARBON: {
       name: "Carbon",
