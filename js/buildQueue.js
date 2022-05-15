4d4807223b623e6d4e927f7fd15fbf84feb04e39
Denis Novik <riskabeker@gmail.com>
2022-05-15 16:38:10 +0300

    - Added ability to specify number of buildings

--- js/buildQueue.js
+++ js/buildQueue.js
@@ -9,12 +9,12 @@ const BuildQueue = {
     if (BuildQueue.isPresent(building)) {
       BuildQueue.changeNumber(building, BuildQueue.operation.ADD);
     } else {
-      BuildQueue.queue.push(building);
+      BuildQueue.queue.push({
+        building: building,
+        number: 1
+      });
       BuildQueue.render(building);
     }
-  },
-  substract: (building) => {
-
   },
   /**
    * Check if building is present in the queue.
@@ -22,16 +22,7 @@ const BuildQueue = {
    * @param building - javascript object of the building.
    */
   isPresent: (building) => {
-    const queue = BuildQueue.queue;
-
-    for (let buildingInex in queue) {
-      const buildingInQ = queue[buildingInex];
-
-      if (buildingInQ.name === building.name) {
-        return true;
-      }
-    }
-    return false;
+    return BuildQueue.getBuilding(building) !== undefined;
   },
   /**
    * Render building on the UI panel.
@@ -42,13 +33,57 @@ const BuildQueue = {
     const $building = document.createElement("div");
     $building.classList.add("building");
     $building.classList.add(Building.getUid(building));
-    $building.innerHTML = building.name;
+    const $buildName = document.createElement("span");
+    $buildName.innerHTML = building.name;
+    $building.append($buildName);
+    const $buildNum = document.createElement("div");
+    $buildNum.innerHTML = 1;
+    $buildNum.classList.add("build-number");
+    $building.append($buildNum);
 
     const $queue = document.getElementById("queue");
     $queue.append($building);
   },
+  /**
+   * Change number of selected buildings.
+   *
+   * @param building - javascript object of the building.
+   * @param operation one of the BuildQueue.operation
+   */
   changeNumber: (building, operation) => {
+    const buildingInQ = BuildQueue.getBuilding(building);
+    const $queue = document.getElementById("queue");
+    const $building = $queue.querySelector("." + Building.getUid(building));
 
+    if (operation === BuildQueue.SUBSTRACT) {
+      buildingInQ.number -= 1;
+    } else {
+      buildingInQ.number += 1;
+    }
+    if (buildingInQ.number < 1) {
+      $building.remove();
+      return;
+    }
+    $building.querySelector(".build-number").innerHTML = buildingInQ.number;
+    return;
+  },
+  /**
+   * Get queue element that corresponds the building.
+   *
+   * @param building - javascript object of the building.
+   * @return object with two fields - building and number.
+   */
+  getBuilding: (building) => {
+    const queue = BuildQueue.queue;
+
+    for (let buildingInex in queue) {
+      const buildingInQ = queue[buildingInex];
+
+      if (buildingInQ.building.name === building.name) {
+        return buildingInQ;
+      }
+    }
+    return;
   },
   operation: {
     SUBSTRACT: "substract",
