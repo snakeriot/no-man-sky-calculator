6185707802a4338ee6982adf689b7de71c8452dc
Denis Novik <riskabeker@gmail.com>
2022-05-16 21:01:10 +0300

    - Added more buildings and ingridients

--- js/enum/buildings.js
+++ js/enum/buildings.js
@@ -11,6 +11,41 @@ const Buildings = {
       number: 25
     }]
   },
+  TIMBER_FLOOR: {
+    name: "Timber Floor",
+    icon: "tibmer_basic_floor.webp",
+    cathegory: MenuItems.BASIC_COMPONENTS.children.TIMBER,
+    recipe: [{
+      ingridient: Ingridients.CARBON,
+      number: 10
+    }]
+  },
+  TIMBER_RECTANGULAR_DOOR: {
+    name: "Timber Rectangular Door",
+    icon: "tibmer_rectangular_door.webp",
+    cathegory: MenuItems.BASIC_COMPONENTS.children.TIMBER,
+    recipe: [{
+      ingridient: Ingridients.CARBON,
+      number: 25
+    },
+    {
+      ingridient: Ingridients.FERRIT_P,
+      number: 10
+    }]
+  },
+  LARGE_TIMBER_TRIPPLE_WINDOW: {
+    name: "Large Timber Triple Window",
+    icon: "large_timber_triple_window.webp",
+    cathegory: MenuItems.BASIC_COMPONENTS.children.TIMBER,
+    recipe: [{
+      ingridient: Ingridients.CARBON,
+      number: 25
+    },
+    {
+      ingridient: Ingridients.GLASS,
+      number: 1
+    }]
+  },
   TIMBER_WALL_LARGE_WINDLOW: {
     name: "Extruded Timber Large Window",
     icon: "timber_extruded_window_wall.webp",
@@ -23,6 +58,30 @@ const Buildings = {
       number: 1
     }]
   },
+  TIMBER_FRAMED_GLASS_PANEL: {
+    name: "Timber-Framed Glass Panel",
+    icon: "timber_framed_glass_panel.webp",
+    cathegory: MenuItems.BASIC_COMPONENTS.children.TIMBER,
+    recipe: [{
+      ingridient: Ingridients.CARBON,
+      number: 10
+    }, {
+      ingridient: Ingridients.GLASS,
+      number: 1
+    }]
+  },
+  TIMBER_RAMP: {
+    name: "Timber Ramp",
+    icon: "timber_ramp.webp",
+    cathegory: MenuItems.BASIC_COMPONENTS.children.TIMBER,
+    recipe: [{
+      ingridient: Ingridients.CARBON,
+      number: 35
+    }, {
+      ingridient: Ingridients.FERRIT,
+      number: 10
+    }]
+  },
   TRADE_TERMINAL: {
     name: "Galactic Trade Terminal",
     icon: "trade_terminal.webp",
@@ -33,6 +92,30 @@ const Buildings = {
     }, {
       ingridient: Ingridients.MICROCHIP,
       number: 3
+    }]
+  },
+  BASE_COMPUTER: {
+    name: "Base Computer",
+    icon: "base_computer.webp",
+    cathegory: MenuItems.EQUIPMENT.children.PORTABLE,
+    recipe: [{
+      ingridient: Ingridients.CHROMATIC_METAL,
+      number: 30
+    }]
+  },
+  BASE_TELEPORT: {
+    name: "Base Teleport Module",
+    icon: "base_teleport.webp",
+    cathegory: MenuItems.EQUIPMENT.children.PERMANENT,
+    recipe: [{
+      ingridient: Ingridients.METAL_PLATING,
+      number: 4
+    }, {
+      ingridient: Ingridients.CARBON_NANOTUBES,
+      number: 2
+    }, {
+      ingridient: Ingridients.SODIUM,
+      number: 40
     }]
   },
   /**
--- js/enum/ingridients.js
+++ js/enum/ingridients.js
@@ -14,9 +14,33 @@ const Ingridients = {
     name: "Glass",
     icon: "glass.webp"
   },
+  FERRIT: {
+    name: "Ferrite Dust",
+    icon: "ferrite.webp"
+  },
   FERRIT_M: {
     name: "Magnetised Ferrite",
     icon: "magnetised_ferrite.webp"
+  },
+  FERRIT_P: {
+    name: "Pure Ferrite",
+    icon: "pure_ferrite.webp"
+  },
+  CHROMATIC_METAL: {
+    name: "Chromatic Metal",
+    icon: "chromatic_metal.webp"
+  },
+  SODIUM: {
+    name: "Sodium",
+    icon: "sodium.webp"
+  },
+  CARBON_NANOTUBES: {
+    name: "Carbon Nanotubes",
+    icon: "carbon_nanotubes.webp"
+  },
+  METAL_PLATING: {
+    name: "Metal Plating",
+    icon: "metail_plating.webp"
   },
   /**
    * Get url of the icon for the ingridient.
